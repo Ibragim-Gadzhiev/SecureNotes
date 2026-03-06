45de1cda6777940f233734041061ff6e632623a5
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2026-03-06 23:58:54 +0300

    feat(auth): implement LoginRequest with validation constraints

--- src/main/java/com/securenotes/dto/request/LoginRequest.java
+++ src/main/java/com/securenotes/dto/request/LoginRequest.java
@@ -1,4 +1,13 @@
 package com.securenotes.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
 }
