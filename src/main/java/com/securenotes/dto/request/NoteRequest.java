50c284951bdaffeffeb8ea50d43e188244cb166b
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2026-03-06 23:59:37 +0300

    feat(note): implement NoteRequest with validation constraints

--- src/main/java/com/securenotes/dto/request/NoteRequest.java
+++ src/main/java/com/securenotes/dto/request/NoteRequest.java
@@ -1,4 +1,16 @@
 package com.securenotes.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class NoteRequest {
+    @NotBlank(message = "Title is required")
+    @Size(max = 255, message = "Title must be less than 255 characters")
+    private String title;
+
+    @NotBlank(message = "Content is required")
+    @Size(max = 10000, message = "Content must be less than 10000 characters")
+    private String content;
 }
